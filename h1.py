1073125d8621556efa7ce8f93ecda1ffb26b4423
heidi520 <heidi.hao@quantacn.com>
2020-05-07 08:42:55 +0800

    修改了代码

--- h1.py
+++ h1.py
@@ -5,3 +5,10 @@ print("hello world")
 print("hello heidi")
 print("heidi is a good girl")
 print("heidi is a nice girl")
+
+for i in range(10):
+  sum = 0
+  if i < 10:
+     sum += i;
+print("sum")
+   
